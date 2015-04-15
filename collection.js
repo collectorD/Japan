75c96ceb2a6af1c2ccbc7fad190394b3c6cd7c82
collectorD <collectorD@yandex.com>
2015-04-15 15:55:37 +0200

    Update collection.js

--- collection.js
+++ collection.js
@@ -1,8 +1,8 @@
 [
 { "moduleId" : "coolmapper02/drivetraffic.jp"},
 { "moduleId" : "wazemap/waze"},
-{ "moduleId" : "Fritz911/osm"},
+{ "moduleId" : "jamesbond002/osm"},
 { "moduleId" : "maps4all/mapquest"},
-{ "moduleId" : "wikifan/wikimap"},
+{ "moduleId" : "wazemap/wikimap"},
 { "moduleId" : "grabowCommuter/GoogleSatTraffic"}
 ]
