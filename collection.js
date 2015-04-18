bfd106cebf4948e50abe535a736fab975e0be37a
collectorD <collectorD@yandex.com>
2015-04-18 08:32:03 +0200

    Update: Link to mapquest repaired

--- collection.js
+++ collection.js
@@ -2,7 +2,7 @@
 { "moduleId" : "coolmapper02/drivetraffic.jp"},
 { "moduleId" : "wazemap/waze"},
 { "moduleId" : "jamesbond002/osm"},
-{ "moduleId" : "maps4all/mapquest"},
+{ "moduleId" : "coolmapper01/mapquest"},
 { "moduleId" : "wazemap/wikimap"},
 { "moduleId" : "grabowCommuter/GoogleSatTraffic"}
 ]
